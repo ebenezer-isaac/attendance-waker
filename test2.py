b0427515d1da1ff86fd16a55a4f66b27f6c6bd3c
Ebenezer Isaac <ebnezr.isaac@gmail.com>
2021-07-28 11:11:20 +0530

    testing

--- test2.py
+++ test2.py
@@ -1,9 +1,6 @@
 import speech_recognition as sr
 import asyncio
 
-
-
-
 # for index, name in enumerate(sr.Microphone.list_microphone_names()):
 #     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 
