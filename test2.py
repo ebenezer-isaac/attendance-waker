52f4efc75d57f40d9c15980d1e691ca66fe29d2a
Ebenezer Isaac <mail@ebenezer-isaac.com>
2021-07-28 11:01:29 +0530

    testing

--- test2.py
+++ test2.py
@@ -3,6 +3,7 @@ import asyncio
 
 
 
+
 # for index, name in enumerate(sr.Microphone.list_microphone_names()):
 #     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 
