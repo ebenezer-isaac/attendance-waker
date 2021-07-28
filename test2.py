53a1c4bab72f2015b9c29d0f699ebecc31d75a89
Ebenezer Isaac <mail@ebenezer-isaac.com>
2021-07-28 10:46:29 +0530

    initial commit

---
+++ test2.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+import asyncio
+
+# for index, name in enumerate(sr.Microphone.list_microphone_names()):
+#     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
+r = sr.Recognizer()
+while(True):
+    with sr.Microphone() as source:   # using microphone here, would like to use speaker instead
+        print("Talk Something...")
+        audio=r.listen(source)
+        print("got the audio")
+        try:
+            text = r.recognize_google(audio)
+            print(text)
+        except Exception as e:
+            print(e)
+            print("Didn't understand the audio")
